8c88bd1bdfef2170a3085c2c2cd9c725cce9d46d
motors nsk <motors@live.ru>
2019-06-14 21:29:37 +0700

    test commit

--- src/test/java/SendMailTest.java
+++ src/test/java/SendMailTest.java
@@ -13,6 +13,7 @@ public class SendMailTest {
         yandexMailPage.refreshCheck();
         yandexMailPage.deleteLastMail();
         // yandexMailPage.other();
+        System.out.println(666);
 
         yandexMailPage.close();
     }
